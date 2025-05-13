613b58682e1a1d0aa8e74ac284df9434891a454c
Reinaldo <you@example.com>
2025-05-12 21:07:50 -0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import noContentImage from "../public/dashboard-nocontent.png";
+export default function Home() {
+  const [hasContent, setHasContent] = useState<boolean>();
+  useEffect(() => {
+    setHasContent(false); // só executa uma vez após o componente montar
+  }, []);
+
+  return hasContent ? (
+    <div>Oi migos</div>
+  ) : (
+    <div className="w-screen h-screen items-center flex justify-center">
+      <Image alt="nao há chamados" src={noContentImage} quality={100} />
+    </div>
+  );
+}
