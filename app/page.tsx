9778a3015657f3ed80d66c603de27b26dc37fff7
Reinaldo <you@example.com>
2025-05-13 21:28:22 -0300

    começando a criar os tickets

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,24 @@ import noContentImage from "../public/dashboard-nocontent.png";
 export default function Home() {
   const [hasContent, setHasContent] = useState<boolean>();
   useEffect(() => {
-    setHasContent(false); // só executa uma vez após o componente montar
+    setHasContent(true); // só executa uma vez após o componente montar
   }, []);
 
   return hasContent ? (
-    <div>Oi migos</div>
+    <div className="text-black pt-9 px-6">
+      <header className="w-full bg-gray-900 rounded-lg  text-white">
+        <div className="flex gap-6 pl-6 py-3">
+          <span>
+            Chamados
+          </span>
+          <span>
+            Chamados
+          </span>
+        </div>
+      </header>
+      
+    
+    </div>
   ) : (
     <div className=" w-screen items-center flex  mt-44 justify-center">
       <Image alt="nao há chamados" src={noContentImage} quality={100} />
