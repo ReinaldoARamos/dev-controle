71740ca1dd4eb523d4829dd473268b8cdd1b27d9
Reinaldo <Gamersolitavi4l@gmail.com>
2025-06-05 21:13:39 -0300

    criando o arquivo de configuração com o provider

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,3 +1,26 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import GoogleProvider from 'next-auth/providers/google'
-import { AuthAction } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions } from "next-auth";
+
+import PrismaClient from "./prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(PrismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({session, token, user}){
+            session.user = {...session.user, id:user.id } as {
+                id: string,
+                name: string,
+                email: string
+            }
+
+            return session
+    }
+  }
+};
