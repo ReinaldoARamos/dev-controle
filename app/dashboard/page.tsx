9053912ca44586baa0eceb7c09081c31d268a1ac
Reinaldo <Gamersolitavi4l@gmail.com>
2025-06-24 20:00:33 -0300

    removendo o useEffect e tornando o dashboard em um server component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,61 +1,31 @@
-
-
-
 import noContentImage from "../../public/dashboard-nocontent.png";
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 import { Tickets } from "../components/Tickets/Tickets";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
 import { redirect } from "next/navigation";
-export default async function Home() {
 
-
-  const session = await  getServerSession(authOptions)
-
- 
-  
-  if(!session || !session.user){
-    redirect('/dashboard')
-  }
-  return (
-    <div className=" mt-44 flex w-screen  items-center justify-center">
-      <Image alt="nao há chamados" src={noContentImage} quality={100} />
-    </div>
-  )
-  
+interface HomeProps {
+  hasContent: boolean;
 }
+export default async function Home({ hasContent }: HomeProps) {
+  const session = await getServerSession(authOptions);
 
+  if (!session || !session.user) {
+    redirect("/dashboard");
+  }
 
-
-
-/*
-"use client";
-
-import { useState, useEffect } from "react";
-import noContentImage from "../../public/dashboard-nocontent.png";
-import Image from "next/image";
-import Link from 'next/link'
-import { Tickets } from "../components/Tickets/Tickets";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../lib/auth";
-export default async function Home() {
-  const [hasContent, setHasContent] = useState<boolean>();
-  useEffect(() => {
-    setHasContent(true); // só executa uma vez após o componente montar
-  }, []);
-
-  const session = await  getServerSession(authOptions)
-
+  hasContent = false
   return hasContent ? (
     <div className="px-6  text-black">
-        
       <h1 className="flex w-full justify-between bg-transparent pb-7 pt-[38px]">
         <span className="text-4xl font-bold">Chamados</span>
-      <Link href={'dashboard/new-ticket'}>
-        <button className="rounded-sm bg-blue-500 px-8 py-[6px] font-medium leading-[150%] text-white transition-all duration-300 hover:bg-blue-400 hover:cursor-pointer">
-          Cadastrar
-        </button></Link>
+        <Link href={"dashboard/new-ticket"}>
+          <button className="rounded-sm bg-blue-500 px-8 py-[6px] font-medium leading-[150%] text-white transition-all duration-300 hover:bg-blue-400 hover:cursor-pointer">
+            Cadastrar
+          </button>
+        </Link>
       </h1>
       <div className="flex items-center justify-between px-4 py-2 text-[16px] font-medium uppercase">
         <div className="w-1/3">Clientes</div>
@@ -74,6 +44,3 @@ export default async function Home() {
     </div>
   );
 }
-
-
-* */
