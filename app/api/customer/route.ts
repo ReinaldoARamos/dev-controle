8e33c9f596b22cc36f0d9547615245b1efb0acc9
Reinaldo <Gamersolitavi4l@gmail.com>
2025-06-30 17:42:37 -0300

    rota da api agora faz uma req no banco de dados e usa o prisma client para dar o create com os dados que recebe no body da req

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -1,14 +1,30 @@
 import { authOptions } from "@/app/lib/auth";
+import PrismaClient from '../../lib/prisma'
 import { error } from "console";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const { name, email, phone, adress } = await request.json();
-const session = await getServerSession(authOptions)
-
-if(!session || !session.user){
+    const session = await getServerSession(authOptions)
+    if(!session || !session.user){
     return NextResponse.json({error: "nota authorazied"}, {status: 400})
 }
-  return NextResponse.json({ message: "cadastro de usuário" });
+  const { name, email, phone, adress, userId } = await request.json();
+try {
+    await PrismaClient.customer.create{
+       data: {
+         name;
+        email;
+        phone;
+        adress: adress ? adress : ""; //adress é opcional, caso nao tenha ele coloca string vazia para evitar erros com unfedined;
+        userId: userId
+       }
+         return NextResponse.json({ message: "cliente cadastrado com sucesso"});
+    }
+} catch (err) {
+    return NextResponse.json({èrror: "Failed to create new customer"}, {status:400})
+}
+
+
+
 }
