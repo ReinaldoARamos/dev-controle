ab441f31d28d509c57b4c8922d5f7a3c5a66b920
Reinaldo <Gamersolitavi4l@gmail.com>
2025-06-30 17:27:11 -0300

    colocando uma proteção na rota da api que da erro 400 caso o usuário tente registrar um cliente sem estar logado

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -1,6 +1,14 @@
+import { authOptions } from "@/app/lib/auth";
+import { error } from "console";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    
-    return NextResponse.json({message: "cadastro de usuário"})
+  const { name, email, phone, adress } = await request.json();
+const session = await getServerSession(authOptions)
+
+if(!session || !session.user){
+    return NextResponse.json({error: "nota authorazied"}, {status: 400})
+}
+  return NextResponse.json({ message: "cadastro de usuário" });
 }
