7687db6ce26e57bf51d9e83adc740f814919a279
Reinaldo <Gamersolitavi4l@gmail.com>
2025-07-15 21:05:20 -0300

    criando a rota delete

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -2,7 +2,20 @@ import { authOptions } from "@/app/lib/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import PrismaClient from "../../lib/prisma";
+import { ok } from "assert";
 
+export async function DELETE(request: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const UserId = searchParams.get("id");
+  console.log("id do cliente:", UserId);
+  return NextResponse.json({ ok: true });
+}
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
 
@@ -19,7 +32,7 @@ export async function POST(request: Request) {
         email,
         phone,
         adress: adress ?? "", // <- Agora funciona corretamente
-        UserId,        // <- Certifique-se de usar `userId`, conforme seu modelo
+        UserId, // <- Certifique-se de usar `userId`, conforme seu modelo
       },
     });
 
