2dc8ef118aecb43589596ca671161049559a26e7
Reinaldo <Gamersolitavi4l@gmail.com>
2025-07-15 21:07:22 -0300

    criando a função de delete, por enquanto ela somente retorna o id, nao deleta de fato

--- app/components/ClientsCard/ClientCard.tsx
+++ app/components/ClientsCard/ClientCard.tsx
@@ -1,5 +1,10 @@
+'use client'
 import { CustumerProps} from '../../utils/custumer.type'
 export function ClientCard({custumer} : {custumer: CustumerProps}) {
+
+  function handleDeleteUser(idUser: string) {
+    alert("user deletado " + idUser)
+  }
   return (
     <div className="flex flex-col bg-slate-100 border text-[16px] border-slate-300 rounded-sm pl-2 p-[8.5px] gap-2.5">
       <span>
@@ -14,7 +19,7 @@ export function ClientCard({custumer} : {custumer: CustumerProps}) {
         <b>email: </b>
      {custumer.email}
       </span>
-      <button className=" text-sm bg-red-500 transition-all duration-300 hover:cursor-pointer rounded-sm text-white hover:font-bold hover:bg-red-700 px-3.5 w-21">
+      <button onClick={() => handleDeleteUser(custumer.id)} className=" text-sm bg-red-500 transition-all duration-300 hover:cursor-pointer rounded-sm text-white hover:font-bold hover:bg-red-700 px-3.5 w-21">
         Deletar
       </button>
     </div>
