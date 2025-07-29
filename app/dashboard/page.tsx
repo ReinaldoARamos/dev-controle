471d31fb79ceece586dfa1e4931d1f3a17d1636a
Reinaldo <Gamersolitavi4l@gmail.com>
2025-07-28 21:14:20 -0300

    criando o root props para ver se o erro para

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,40 +7,38 @@ import { authOptions } from "../lib/auth";
 import { redirect } from "next/navigation";
 
 import { RefreshButton } from "./components/ButtonRefresh";
-import PrismaClient from '../lib/prisma'
+import PrismaClient from "../lib/prisma";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   const tickets = await PrismaClient.ticket.findMany({
     where: {
-     
       status: "ABERTO",
       customer: {
-        UserId: session?.user.id
-      }
+        UserId: session?.user.id,
+      },
     },
     include: {
       customer: true,
     },
-    orderBy : {
-      created_at: "desc"
-    }
+    orderBy: {
+      created_at: "desc",
+    },
   });
 
   return tickets.length != 0 ? (
     <div className="px-6  text-black">
       <h1 className="flex w-full justify-between bg-transparent pb-7 pt-[38px]">
         <span className="text-4xl font-bold">Chamados</span>
-     <div className="flex gap-2 items-center justify-center">
-       
-        <Link href={"dashboard/new-ticket"}>
-          <button className="rounded-sm bg-blue-500 px-8 py-[6px] font-medium leading-[150%] text-white transition-all duration-300 hover:bg-blue-400 hover:cursor-pointer">
-            Cadastrar
-          </button>
-        </Link>
+        <div className="flex gap-2 items-center justify-center">
+          <Link href={"dashboard/new-ticket"}>
+            <button className="rounded-sm bg-blue-500 px-8 py-[6px] font-medium leading-[150%] text-white transition-all duration-300 hover:bg-blue-400 hover:cursor-pointer">
+              Cadastrar
+            </button>
+          </Link>
           <RefreshButton />
-     </div>
+        </div>
       </h1>
       <div className="flex items-center justify-between px-4 py-2 text-[16px] font-medium uppercase">
         <div className="w-1/3">Clientes</div>
@@ -50,6 +48,7 @@ export default async function Home() {
       </div>
 
       {tickets.map((ticket) => (
+        // @ts-ignore
         <Tickets key={ticket.id} tickets={ticket} customer={ticket.customer} />
       ))}
     </div>
